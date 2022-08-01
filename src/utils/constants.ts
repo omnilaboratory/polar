9726f53837c46e1f3b2d2390e3fadb0c5c314df4
guoyalei <guoyalei@fyixin.com>
2022-08-01 18:24:54 +0800

    update obd node version

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -303,8 +303,8 @@ export const defaultRepoState: DockerRepoState = {
       },
     },
     obd: {
-      latest: '0.0.3',
-      versions: ['0.0.3'],
+      latest: '0.0.7',
+      versions: ['0.0.7'],
     },
     'c-lightning': {
       // latest: '0.10.0',
